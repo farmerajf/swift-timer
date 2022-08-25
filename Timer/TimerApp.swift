73a8779f7b484fcad2e8c623df165b800de34e1f
Adam Farmer <adam@adamfarmer.dev>
2022-08-25 17:15:39 +1000

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Adam Farmer on 25/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
